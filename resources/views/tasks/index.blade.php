5c6afbf1d1b3a34cef4e3bb25a6891c5e24d1d5d
kazuma.suzuki <kazuma.001600@gmail.com>
2020-07-02 14:06:40 +0000

    update

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -14,7 +14,7 @@
       <tbody>
         @foreach ($tasks as $task)
         <tr>
-          <td>{!! link_to_route("tasks.show",$task->id, ["task" => $task->id]) !!}</td>
+          <td>{!! link_to_route("tasks.show",$task->user_id, ["task" => $task->id]) !!}</td>
           <td>{{ $task->status }}</td>
           <td>{{ $task->content }}</td>
         </tr>
